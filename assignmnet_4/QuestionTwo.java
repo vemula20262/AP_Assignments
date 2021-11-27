c0611e37933972533963b5f3aa947b1f1c5f0a99
vemula20262 <88724050+vemula20262@users.noreply.github.com>
2021-11-27 19:34:23 +0530

    Update QuestionTwo.java

--- assignmnet_4/QuestionTwo.java
+++ assignmnet_4/QuestionTwo.java
@@ -1,1 +1,21 @@
 
+public class QuestionTwo {
+
+	public static void main(String[] args) {	
+		Integer[][] colorMatrix = { {1,2,3},
+				{4,5,6},{7,8,9},{10,11,12}
+				};
+		
+		Image img = new Image();
+		img.setColorMatrix(colorMatrix);
+		
+		System.out.println(img);
+		img.Negate();
+		System.out.println(img);
+		
+		img.setImageColor(3, 2, 21);
+		System.out.println(img);
+	}
+
+}
+
